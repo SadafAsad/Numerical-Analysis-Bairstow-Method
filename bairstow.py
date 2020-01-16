43d6cb8f7710cea3796bbe821e960926ec23b6d4
sadaf <sadaf98x@gmail.com>
2020-01-16 19:59:12 +0330

    b c calculator function fixed

--- bairstow.py
+++ bairstow.py
@@ -8,17 +8,19 @@ def b_c_calculation(k, n, a_b_list, r, s):
         + ( r * b_c_calculation(k+1, n, a_b_list, r, s) ) 
         + ( s * b_c_calculation(k+2, n, a_b_list, r, s) ) )
     
-a_list = [1.0000, 1,0000, 3.0000, 4.0000, 6.0000]
+a_list = [6.0000, 4.0000, 3.0000, 1.0000, 1.0000]
 b_list = [0, 0, 0, 0, 0]
 c_list = [0, 0, 0, 0, 0]
 
 i=0
-while(i<7):
+while(i<5):
     b_list[i] = b_c_calculation(i, 4, a_list, -2.1, -1.9)
+    i = i+1
 
 i=0
-while(i<7):
+while(i<5):
     c_list[i] = b_c_calculation(i, 4, b_list, -2.1, -1.9)
+    i = i+1
 
 print(a_list)
 print(b_list)
